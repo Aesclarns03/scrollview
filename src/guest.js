6e02c504064dcc185d1e3fd7467cabf30f260e55
Aesclarns03 <144084175+Aesclarns03@users.noreply.github.com>
2023-11-10 06:51:01 +0800

    scrollview

---
+++ src/guest.js
@@ -0,0 +1,82 @@
+import react from "react";
+
+export const guest = [
+    {
+        id: 1,
+        name: "JESSA MAE RESMA",
+        place: "Egypt, Tagbilaran"
+    },
+    {
+        id: 2,
+        name: "ALDWIN, HANDUMON",
+        place: "Canada, Jagna"
+    },
+    {
+        id: 3,
+        name: "MAGALONA, JOHNBHERT CLINT",
+        place: "Cortes, Lourdes"
+    },
+    {
+        id: 4,
+        name: "RENOBLAS, IRA",
+        place: "Japan, Hanopol"
+    },
+    {
+        id: 5,
+        name: "JAGUNOS, JOANA",
+        place: "Brazil, Hanopol"
+    },
+    {
+        id: 6,
+        name: "JAPORGS, HYKE",
+        place: "Jordan, Sinai"
+    },
+    {
+        id: 7,
+        name: "SINCHAS, ",
+        place: "Japan, Hanopol"
+    },
+    {
+        id: 8,
+        name: "WASHAMIY, DINDA",
+        place: "Nigeria, Catigbian"
+    },
+    {
+        id: 9,
+        name: "MONALIZA, LEPARTO",
+        place: "Los Angeles, California"
+    },
+    {
+        id: 10,
+        name: "LIA, REFUGIO",
+        place: "Argentina, Nevada"
+    },
+    {
+        id: 11,
+        name: "SHEMY, SHEIN",
+        place: "Alaska, Bandaria"
+    },
+    {
+        id: 12,
+        name: "JESSELE, LOMOD",
+        place: "Japan, Hanopol"
+    },
+    {
+        id: 13,
+        name: "LOPEZ, DERMA",
+        place: "France, Paris"
+    },
+    {
+        id: 14,
+        name: "DENMARK, LINDA",
+        place: "United, states"
+    },
+    {
+        id: 15,
+        name: "KING, JEYPEE",
+        place: "Japan, Hanopol"
+    },
+
+]
+
+export default guest;
